d1b42c00f23cebe71d30ab6e4db138c8e3621a05
Andres Malagon <andresmalagon@Andress-MacBook-Pro.local>
2022-01-14 09:18:15 -0500

    post model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -37,9 +37,8 @@ router.get('/:id', (req, res) => {
 
 });
 
-//POST /api/users
 router.post('/', (req, res) => {
-    //expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -50,6 +49,33 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+
+//POST /api/users
+router.post('/login', (req, res) => {
+    //expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        //Verify user
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+
+    });
 });
 
 //PUT /api/users
